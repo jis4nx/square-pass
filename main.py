d7d5483781046f96c15f1c316638e6ca09260d95
TheGreatestShoaib <shoaibmadeinbangladesh@gmail.com>
2021-12-13 15:46:26 +0600

    Dashboard Placement (early state)

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import sys
 from passwordManager import base
+import argparse
+
+from colorama import Fore
+
+green = Fore.GREEN
+white = Fore.WHITE
+red = Fore.RED
+
+
+
+banner = f"""
+
+      {green}   / / {red}(_)_ __  ___  ___  ___ _   _ _ __ ___  {white}            .
+      {green}  / /  {red}| | '_ \/ __|/ _ \/ __| | | | '__/ _ \ {white}            .
+    {green} _ / / {red}  | | | | \__ \  __/ (__| |_| | | |  __/{white}             .
+    {green}(_)_/    {red}|_|_| |_|___/\___|\___|\__,_|_|  \___|{white}             .
+
+""".center(2)
+
+
+
 
 pwdwrong = ["Sorry that's not correct!",
             "Not even close!",
@@ -7,44 +28,70 @@ pwdwrong = ["Sorry that's not correct!",
             "Hold on and give the correct password!"
             ]
 
-DASH = """
-------------------Help-------------------------
 
-python main.py update facebook
 
-python main.py add
-
-python main.py -u username -a appname
-
-python main.py showall -sort ascend / date
-
-python main.py backup
-
-------------0000000000000----------------------
-//
-------------000000000000-----------------------
---copytoclip
---hint
-------------------------------------------------
-"""
-action = sys.argv[1]
+#action = sys.argv[1]
 
 db =base.DatabaseManager("shoaibislam")
 
-if action == "help":
-    print(DASH)
-elif action == "add":
-    db.insert()
-elif action == "-a":
-    act_name = sys.argv[2]
-    print(act_name)
-    db.viewdb_by_appname(act_name)
-elif action == "-u":
-    act_name = sys.argv[2]
-    db.viewdb_by_username(act_name)
+parser = argparse.ArgumentParser(prog="ins",
+                                 formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description=banner,
+                                 epilog = "breh this will be the bottom line",
+                                 conflict_handler="resolve",
+                                 usage= "%(prog)s action [show|remove|search] "
 
-elif action == "showall":
+                                 )
+
+flags = parser.add_argument_group('Command :', '')
+
+opt = parser.add_argument_group('Options :', '')
+
+
+
+
+flags.add_argument("--ls","--showlist",action="store_const",help="Shows Credentials",const="all")
+flags.add_argument("--rm","--remove",action="store_const",help="remove a credential",const="3")
+flags.add_argument("--Ss","--search", metavar="" ,help="search trough credentials")
+flags.add_argument("--t","--touch", action="store_true", help="Add new credential")
+
+opt.add_argument("--u","--username",metavar="", help="Filter by Username")
+opt.add_argument("--a" ,"--appname",metavar="",help="Filter by Appname")
+opt.add_argument("--c","--copy",action="store_true",help="Copy Pass to clip board")
+opt.add_argument("--r",'--recent',action="store_true",help="Show recently modified credentials")
+opt.add_argument("--w",'--warn',action="store_true",help="warn about weak passwords")
+
+
+dan = parser.add_argument_group('Often Args :', '')
+
+dan.add_argument("--bigbang",action="store_true",help="Erase all information")
+
+                    
+
+
+args = parser.parse_args()
+
+
+if args.ls:
     db.viewall()
+if args.t:
+    db.insert()
 
-else:
-    print(action)
+
+# if action == "help":
+    # print(DASH)
+# elif action == "add":
+    # db.insert()
+# elif action == "-a":
+    # act_name = sys.argv[2]
+    # print(act_name)
+    # db.viewdb_by_appname(act_name)
+# elif action == "-u":
+    # act_name = sys.argv[2]
+    # db.viewdb_by_username(act_name)
+
+# elif action == "showall":
+    # db.viewall()
+
+# else:
+    # print(action)
